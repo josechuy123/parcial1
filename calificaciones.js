14e33d69d99a8e0fd688d0514e8537de3fdf5c9c
josechuy123 <60717144+josechuy123@users.noreply.github.com>
2020-02-10 17:44:43 -0500

    calificaciones
    con if anidades if else ponemos las condiciones y si no se cuplen van escalando imprimiendo su valor ingles

---
+++ calificaciones.js
@@ -0,0 +1,31 @@
+var num1=Math.floor(Math.random()*10)+1;
+console.log(num1)
+if(num1==10)
+    console.log("A");
+        else if(num1==9)
+            console.log("B");
+
+if(num1==8)
+    console.log("C");
+        else if(num1==7)
+        console.log("D");
+if(num1==6)
+    console.log("E");
+        else if(num1<6)
+            console.log("F");
+
+/*if(/**numero/ ){
+    console.log("");
+}else{
+    if(9){
+        console.log("");
+    }else{
+        if(){
+            console
+        }else{
+            if(){
+
+            }
+        }
+    }
+}*/
