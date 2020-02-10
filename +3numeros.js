43a64b887997b303a8f898eb10dbe096abac539b
josechuy123 <60717144+josechuy123@users.noreply.github.com>
2020-02-10 17:01:35 -0500

    mayor 3 numeros
    en este programa con if anidados y declarando 3 numeros aleatorios con if anidados evaluando dictamina e imprimer el mayor de los 3

---
+++ +3numeros.js
@@ -0,0 +1,19 @@
+var num1=Math.floor(math.random()*10);
+var num2=Math.floor(math.random()*10);
+var num3=Math.floor(math.random()*10);
+
+if(num1>num2 && num1>num3){
+    console.log("Es el mayor");
+    if(num2>num1 && num2>num3)
+    {
+        console.log(" El seugndo es el mayor");
+    }
+
+}
+else{
+    console.log("el tecero es el mayor")
+}
+
+
+
+
